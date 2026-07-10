92c6dfe2112dba7cc478b4756c1275cb34957129
Maarten de Vrijer <m.devrijer@peute.nl>
2026-07-10 11:44:53 +0200

    fix voor railway

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template_string, request
 import requests
 
@@ -104,4 +105,4 @@ def index():
     return render_template_string(HTML, bedrijven=bedrijven, stad=stad, categorie=categorie, extra=extra)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
